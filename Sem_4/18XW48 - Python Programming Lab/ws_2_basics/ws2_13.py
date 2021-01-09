acc4a081126e73d510f8e95801da32e3e9f61c6b
BattleBaxx <54268057+Ranga1503@users.noreply.github.com>
2021-01-09 13:53:28 +0530

    added operations research

--- Sem_4/18XW48 - Python Programming Lab/ws_2_basics/ws2_13.py
+++ Sem_4/18XW48 - Python Programming Lab/ws_2_basics/ws2_13.py
@@ -9,10 +9,13 @@ if __name__ == "__main__":
             print("The letter " + guessed + " is alredy guessed.")
             continue
         guessed_letters.append(guessed)
-        indices = [index for index, val in enumerate(word_list) if val == guessed]
+        indices = [index for index, val in enumerate(
+            word_list) if val == guessed]
         if(len(indices) == 0):
-            print("Sorry the guessed letter is not present in the list. " + "The word is now: " + "".join(present_list))
+            print("Sorry the guessed letter is not present in the list. " +
+                  "The word is now: " + "".join(present_list))
         for index in indices:
             present_list[index] = guessed
-        print("You guessed the correct letter !! " + "The word is now: " + "".join(present_list))
+        print("You guessed the correct letter !! " +
+              "The word is now: " + "".join(present_list))
     print("Congrats you have guessed the word !!!!!")
