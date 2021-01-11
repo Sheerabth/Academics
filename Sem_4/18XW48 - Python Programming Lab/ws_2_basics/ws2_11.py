2f6a3ce1da4816664fbe01e4951f593a02b8639f
BattleBaxx <54268057+Ranga1503@users.noreply.github.com>
2021-01-11 08:41:32 +0530

    added ws2_11.py

--- Sem_4/18XW48 - Python Programming Lab/ws_2_basics/ws2_11.py
+++ Sem_4/18XW48 - Python Programming Lab/ws_2_basics/ws2_11.py
@@ -1,6 +1,29 @@
-if __name__ == "__main__":
-    input_list = input("Please enter the string: ").split(" ", 1)
-    if input_list[0] == "print":
-        print(input_list[1])
-    else:
-        
+def execute(variables,expression):
+    try:
+        return eval(expression,variables)
+    except NameError:
+        print('Value not defined')
+        return 
+
+
+if __name__ == '__main__':
+    print('Enter the command - ')
+    variables = {}
+    try:
+        while 1 :
+            command = input('Please enter the input')
+            if 'print' in command.lower():
+                expression = command.split('print ')
+                result = execute(variables,expression[1])
+                print(result)
+            elif 'let' in command.lower():
+                equal_index = command.find('=')
+                let_index = command.find('let')
+                value = execute(variables,command[equal_index+1 :])
+                variables[command[let_index+4:equal_index-1]] = value
+            elif command == 'clear':
+                variables.clear()
+            elif command == 'exit':
+                break
+    except KeyboardInterrupt:
+        print(' Keyboard Interrupt!')
