2b26f6a0fb567efbb957b52ff6459930b4252459
Sheerabth <sheerabth1610@gmail.com>
2022-02-26 10:06:53 +0530

    add: affine cipher encryption and decryption

--- sem_6/sic/ps1/ps1_1.py
+++ sem_6/sic/ps1/ps1_1.py
@@ -1,6 +1,9 @@
+from email.encoders import encode_quopri
+from http.cookiejar import CookieJar
+import math
 from string import ascii_lowercase
 
-class affine_cipher:
+class AffineCipher:
     def __init__(self, a, b) -> None:
         self.a = a
         self.b = b
@@ -8,22 +11,38 @@ class affine_cipher:
     def encrypt(self, plain_text):
         enc_text = ""
         for character in plain_text:
-            is_upper = False
+            if not character.isalpha():
+                enc_text += character
+                continue
+
+            result = ((self.a*(ord(character.lower()) - 97) + self.b) % 26) + 97
+
             if character.isupper():
-                is_lower = True
-
-            result = self.a*(ord(character.lower()) - 97) + self.b
-
-            if is_upper:
                 enc_text += chr(result).upper()
             else:
                 enc_text += chr(result)
+
         return enc_text
 
     def decrypt(self, enc_text):
-        pass
+        dec_text = ""
+        for character in enc_text:
+            if not character.isalpha():
+                dec_text += character
+                continue
 
-class keyword_cipher:
+            result = int((math.pow(self.a, -1)*(ord(character.lower()) - 97)) % 26) + 97
+            print(result)
+
+            if character.isupper():
+                dec_text += chr(result).upper()
+            
+            else:
+                dec_text += chr(result)
+
+        return dec_text
+
+class KeywordCipher:
     def __init__(self, keyword) -> None:
         keyword = keyword.lower()
         self.enc_key = keyword
@@ -51,7 +70,12 @@ class keyword_cipher:
 
         return dec_text
 
-class columnar_transposition:
+class ColumnarTransposition:
     def __init__(self) -> None:
         pass
         
+
+if __name__ == "__main__":
+    affine_cipher = AffineCipher(5, 8)
+    encrypted = affine_cipher.decrypt("WZU SAAL")
+    print(encrypted)
--- sem_6/sic/ps1/ps1_4.py
+++ sem_6/sic/ps1/ps1_4.py
@@ -4,4 +4,43 @@ Created on Tue Feb 22 12:03:36 2022
 
 @author: 19pw33
 """
+import math
 
+class AffineCipher:
+    def __init__(self, a, b) -> None:
+        self.a = a
+        self.b = b
+
+    def encrypt(self, plain_text):
+        enc_text = ""
+        for character in plain_text:
+            if not character.isalpha():
+                enc_text += character
+                continue
+
+            result = ((self.a*(ord(character.lower()) - 97) + self.b) % 26) + 97
+
+            if character.isupper():
+                enc_text += chr(result).upper()
+            else:
+                enc_text += chr(result)
+
+        return enc_text
+
+    def decrypt(self, enc_text):
+        dec_text = ""
+        for character in enc_text:
+            if not character.isalpha():
+                dec_text += character
+                continue
+
+            result = int((math.pow(self.a, -1)*(ord(character.lower()) - 97)) % 26) + 97
+            print(result)
+
+            if character.isupper():
+                dec_text += chr(result).upper()
+            
+            else:
+                dec_text += chr(result)
+
+        return dec_text
