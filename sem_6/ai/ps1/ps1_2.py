4957a3bc9bb3c6f090e9bece23952d646506135b
Sheerabth <sheerabth1610@gmail.com>
2022-03-23 22:40:25 +0530

    add: hill climbing

--- sem_6/ai/ps1/ps1_2.py
+++ sem_6/ai/ps1/ps1_2.py
@@ -1,11 +1,5 @@
-from gettext import find
-from multiprocessing.sharedctypes import Value
-from tkinter import N
-from tkinter.messagebox import NO
-from turtle import distance
-from unittest.mock import patch
 from graph import Graph
-from typing import List, Set, Union
+from typing import List, Union
 
 
 class AStar:
@@ -69,6 +63,51 @@ class AStar:
         return path
 
 
+class HillClimbing:
+    def __init__(self, graph: Graph) -> None:
+        self.graph = graph
+        self.utility = dict()
+
+    
+    def calculate_utility(self, start: int, end: int) -> Union[None, int]:
+        if start in self.utility:
+            return self.utility[start]
+
+        if start == end:
+            self.utility[start] = 0
+            return self.utility[start]
+
+        if start not in self.graph.adj_list:
+            self.utility[start] = None
+            return None
+        
+        distances = list()
+        for neighbour in self.graph.adj_list[start]:
+            result = self.calculate_utility(neighbour, end)
+            if result is not None:
+                distances.append(self.utility[neighbour])
+
+        if len(distances) == 0:
+            self.utility[start] = None
+            return None
+
+        self.utility[start] = min(distances) + 1
+        return self.utility[start]
+
+    def find(self, start: int, end: int) -> List[int]:
+        self.calculate_utility(start, end)
+        path = list()
+        path.append(start)
+        current_node = start
+
+        while current_node != end:
+            neighbours_utility = {key: value for (key, value) in self.utility.items() if key in self.graph.adj_list[current_node] and value is not None}
+            current_node = min(neighbours_utility, key=neighbours_utility.get)
+            path.append(current_node)
+
+        return path
+
+
 if __name__ == "__main__":
     g = Graph()
     g.addEdge(0, 1)
@@ -77,8 +116,16 @@ if __name__ == "__main__":
     g.addEdge(2, 5)
     g.addEdge(5, 3)
     g.addEdge(3, 4)
+
+    print("A Star")
     a_star = AStar(g)
     a_star.calculate_utility(0, 4)
     print(a_star.heuristic)
     print(a_star.cost)
     print(a_star.find(0, 4))
+
+    print("Hill Climbing")
+    hill_climbing = HillClimbing(g)
+    hill_climbing.calculate_utility(0, 4)
+    print(hill_climbing.utility)
+    print(hill_climbing.find(0, 4))
